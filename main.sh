b8377c570d3eafb58ccb866432f34df7a518b0ce
life <contact.5lu1v@simplelogin.co>
2022-07-04 11:35:11 +0300

    Ignore hidden directories

--- main.sh
+++ main.sh
@@ -3,12 +3,23 @@
 REPO=/home/user/awecron
 USER=user
 
-# just to make sure, it is essential for script/binary execution
-cd /
+# lists all dirs in the repo into an array except dotdirs
+# this could be useful when modifying something and preventing unexpected execution 
+function exedir() {
+	# gets the inputed string
+	while read -r -d $'\0' line; do
+		# puts into an array
+		arr+=("$line")
+	done < <(find $REPO/*/ -maxdepth 0 -regextype posix-egrep -regex '.*' -print0) # inputs the string
+}
 
 function main() {
-	# runs in all folders in the repo
-	for i in $REPO/*/; do
+
+	exedir
+
+	# runs in folders in the repo
+	for i in ${arr[@]}; do
+		
 		everytime=$(cat $i\everytime)
 		nextrun=$(cat $i\nextrun)
 		rootperms=$(cat $i\rootperms)
@@ -26,7 +37,13 @@ function main() {
 		fi
 	done
 	
+	# cleaning the array
+	exedir=()
 }
+
+
+# just to make sure, it is essential for script/binary execution
+cd /
 
 while true; do
 	main
